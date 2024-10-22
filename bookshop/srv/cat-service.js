054711f417a3b882eadef57b3976108a51607d4c
Venkata Ramireddy <sap.ramireddy@gmail.com>
2024-10-22 09:09:16 +0000

    part15

--- bookshop/srv/cat-service.js
+++ bookshop/srv/cat-service.js
@@ -1,12 +1,29 @@
 console.log("**** Simple Message in BTP Service *****");
 const cds = require('@sap/cds')
+const logger = cds.log('bookshop')
+
 module.exports = cds.service.impl(function () {
-    console.log("**** JS-CDS Anomous function ****");
-    this.on ('READ','Books', (...rest) => { 
-        return [{
-            ID: cds.utils.uuid(),
-            tile:"The Title",
-            author_ID: 2
-        }]
+    logger.log("**** JS-CDS Anomous function ****");
+
+    // this.on ('READ','Books', (req, next) => { 
+    //     console.log("**** Handling Read Books ****");
+    //     return [{
+    //         ID: cds.utils.uuid(),
+    //         tile:"The Title",
+    //         author_ID: 2
+    //     }]
+    // })
+
+    this.after ('READ','Books', (data, req) => { 
+        //logger.log("*** After Event ***", data);
+        //Anomoyous call back functions
+        data.map(book => book.title +='!');
     })
-})
+
+    this.after ('each','Books', (data, req) => { 
+        logger.log("*** Each ***:",data);
+        //Anomoyous call back functions
+        //data.map(book => book.title +='!');
+    })
+
+ })
